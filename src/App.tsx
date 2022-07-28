7d98a09d0f6c11998bdd2eebcd30d3687c444f44
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-07-28 22:49:35 +0530

    app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { createStage } from './utils/helper';
 import { StyledTetrisWrapper, StyledTetris } from './App.styles';
+import Stage from './components/Stage';
+import Display from './components/Display';
+import StartButton from './components/StartButton';
 
 const App: React.FC = () => {
+  const [dropTime, setDroptime] = React.useState<null | number>(null)
+  const [gameOver, setGameOver] = React.useState(true)
+
+  const gameArea = React.useRef<HTMLDivElement>(null)
+
   return (
     <StyledTetrisWrapper role='button' tabIndex={0}>
-      <StyledTetris>Start here!</StyledTetris>
+      <StyledTetris>
+        <div className='display'>
+          {gameOver ? (
+            <>
+              <Display gameOver={gameOver} text='Game Over!' />
+              <StartButton callback={() => null} />
+            </>
+          ) : (
+            <>
+              <Display text={`Score: `} />
+              <Display text={`Rows: `} />
+              <Display text={`Level: `} />
+            </>
+          )}
+        </div>
+        <Stage stage={createStage()} />
+      </StyledTetris>
     </StyledTetrisWrapper>
   )
 }
